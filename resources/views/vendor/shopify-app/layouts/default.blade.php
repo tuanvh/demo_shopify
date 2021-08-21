325abfe6212c3a57329ab8ba2cd46a1ab3872a7d
vu.huy.tuan@sun-asterisk.com <vu.huy.tuan@sun-asterisk.com>
2021-08-21 22:11:05 +0700

    config tailwinCss

--- resources/views/vendor/shopify-app/layouts/default.blade.php
+++ resources/views/vendor/shopify-app/layouts/default.blade.php
@@ -5,6 +5,9 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ \Osiset\ShopifyApp\Util::getShopifyConfig('app_name') }}</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         @yield('styles')
     </head>
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,10 +14,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .options({
-        processCssUrls: false,
-        postCss: [ tailwindcss('./tailwind.config.js') ],
-    });
-    // .postCss("resources/css/app.css", "public/css", [
-    //     require("tailwindcss"),
-    // ]);
+    .postCss("resources/css/app.css", "public/css", [
+        require("tailwindcss"),
+    ]);
